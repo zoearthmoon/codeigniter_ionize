49f8574b106fade00cce8fd7d8b9658012f5bf5d
zoearthmoon@gmail.com <zoearthmoon@gmail.com>
2014-04-01 09:30:56 +0800

    新增一個預設版型頁面(純記錄.未使用)

---
+++ themes/admin_zoearth/views/tpl_page.php
@@ -0,0 +1,13 @@
+<section class="content-header">
+    <div style="float:right;">
+    <a class="btn btn-primary z0-add" href="<?php echo sayLink::say('menu/create')?>">
+        <i class="icon-pencil"></i><?php echo lang('add_new_menu')?>
+    </a>
+    </div>
+    <?php echo sayPathLink::show();?>
+</section>
+<section class="content">
+<div id="maincolumn">
+
+</div>
+</section>
