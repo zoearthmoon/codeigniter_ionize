405f898d3b37b12725ef457be007cba3aef88c98
zoearthmoon@gmail.com <zoearthmoon@gmail.com>
2014-04-07 10:18:52 +0800

    新增 中文語系檔案

---
+++ filemanager/lang/zh_TW.php
@@ -0,0 +1,77 @@
+<?php
+define('lang_Select','選擇');
+define('lang_Erase','刪除');
+define('lang_Open','開放');
+define('lang_Confirm_del','您確定要刪除這個文件？');
+define('lang_All','全部');
+define('lang_Files','文件');
+define('lang_Images','圖像');
+define('lang_Archives','檔案');
+define('lang_Error_Upload','上傳的文件超過允許的最大大小。');
+define('lang_Error_extension','文件擴展名是不允許的。');
+define('lang_Upload_file','上傳');
+define('lang_Filters','過濾器');
+define('lang_Videos','視頻');
+define('lang_Music','音樂');
+define('lang_New_Folder','新建文件夾');
+define('lang_Folder_Created','文件夾中創建正確');
+define('lang_Existing_Folder','現有的文件夾');
+define('lang_Confirm_Folder_del','你確定要刪除該文件夾及其中的所有元素？');
+define('lang_Return_Files_List','返回文件列表');
+define('lang_Preview','預覽');
+define('lang_Download','下載');
+define('lang_Insert_Folder_Name','插入文件夾名稱');
+define('lang_Root','根');
+define('lang_Rename','重命名');
+define('lang_Back','回');
+define('lang_View','查看');
+define('lang_View_list','列表視圖');
+define('lang_View_columns_list','列列表視圖');
+define('lang_View_boxes','箱視圖');
+define('lang_Toolbar','工具欄');
+define('lang_Actions','操作');
+define('lang_Rename_existing_file','該文件已經存在');
+define('lang_Rename_existing_folder','文件夾已經存在');
+define('lang_Empty_name','這個名字是空');
+define('lang_Text_filter','文本過濾器');
+define('lang_Swipe_help','重擊文件/文件夾的名稱,以顯示選項');
+define('lang_Upload_base','基地上傳');
+define('lang_Upload_java',' JAVA上傳(大尺寸文件）');
+define('lang_Upload_java_help','如果Java小程序不加載, 1,確保你已經安裝了Java,否則<a href="http://java.com/en/download/"> [下載鏈接] </a> 2確保沒有被你的防火牆"）';
+define('lang_Upload_base_help','拖放文件或單擊區域的上方(現代瀏覽器）,然後選擇文件(S ）當上傳完成後,點擊"返回到文件列表"按鈕。"）';
+define('lang_Type_dir','目錄');
+define('lang_Type','類型');
+define('lang_Dimension','尺寸');
+define('lang_Size','大小');
+define('lang_Date','日期');
+define('lang_Filename','文件名');
+define('lang_Operations','操作');
+define('lang_Date_type',' Y-M- D');
+define('lang_OK',' OK');
+define('lang_Cancel','取消');
+define('lang_Sorting','排序');
+define('lang_Show_url','顯示的URL');
+define('lang_Extract','這裡提取');
+define('lang_File_info','文件信息');
+define('lang_Edit_image','編輯圖像');
+define('lang_Duplicate','重複');
+define('lang_Folders','文件夾');
+define('lang_Copy','複製');
+define('lang_Cut','切');
+define('lang_Paste','粘貼');
+define('為lang_cb',' CB'); //剪貼板
+define('lang_Paste_Here','粘貼到這個目錄');
+define('lang_Paste_Confirm','您確定要粘貼到這個目錄,如果遇到這將覆蓋現有的文件/文件夾？');
+define('lang_Paste_Failed','無法粘貼文件(S ）');
+define('lang_Clear_Clipboard','清除剪貼板');
+define('lang_Clear_Clipboard_Confirm','您確定要清除剪貼板？');
+define('lang_Files_ON_Clipboard','有剪貼板上的文件。');
+define('lang_Copy_Cut_Size_Limit','所選擇的文件/文件夾太大, ％s的限制： ％d個MB /操作'); // ％ S =剪切或複製
+define('lang_Copy_Cut_Count_Limit','您選擇了太多的文件/文件夾複製到％s的限制： ％d個文件/操作'); // ％ S =剪切或複製
+define('lang_Copy_Cut_Not_Allowed','你不允許％s的文件。'); // ％ S(1） =剪切或複製的％s( 2 ） =文件或文件夾
+define('lang_Aviary_No_Save','無法保存圖像');
+define('lang_Zip_No_Extract','無法解壓縮文件可能已損壞。');
+define('lang_Zip_Invalid','不支持此擴展有效期： ZIP, GZ,焦油。');
+define('lang_Dir_No_Write','您選擇的目錄不可寫。');
+define('lang_Function_Disabled','在％s的功能已被禁用的服務器。'); // ％ S =剪切或複製
+?>
